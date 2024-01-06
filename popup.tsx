500d41a69f34da630f002c2b8741ab338ca6b032
Nick Reisenauer <nickdreisenauer@gmail.com>
2024-01-05 17:48:21 -0700

    feat: add prettier

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,10 @@
-import { useState } from "react"
-
-function IndexPopup() {
-  const [data, setData] = useState("")
-
+const IndexPopup = () => {
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
-  )
-}
+    <main>
+      <h1>tabop</h1>
+      <button>Hi</button>
+    </main>
+  );
+};
 
-export default IndexPopup
+export default IndexPopup;
